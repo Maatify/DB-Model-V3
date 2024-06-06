aca34f6614d3a1da64ad96077800a1ed57144b96
Mohamed Abdulalim <megyptm@gmail.com>
2024-06-06 11:51:22 +0300

    1.2.010

--- db-model/JoinTablesModel.php
+++ db-model/JoinTablesModel.php
@@ -44,7 +44,22 @@ abstract class JoinTablesModel extends PDOBuilder
         }
         return rtrim($cols, ', ');
     }
-
+    private function generateJoinUniqueColumns(array $columns_with_types): string
+    {
+        $cols = '';
+        foreach ($columns_with_types as $col => $type) {
+            if ($col != $this->identify_table_id_col_name) {
+                $defaultValue = match ($type) {
+                    1 => 0,
+                    2 => "0",
+                    default => "''",
+                };
+                $columnAlias = $col;
+                $cols .= " IFNULL(`$this->tableName`.`$col`, $defaultValue) as $columnAlias, ";
+            }
+        }
+        return rtrim($cols, ', ');
+    }
     private function generateJoin(string $joinType, string $table_name, bool $withAlias = false): array
     {
         $joinClause = " $joinType JOIN `$this->tableName` ON `$this->tableName`.`$this->identify_table_id_col_name` = `$table_name`.`$this->identify_table_id_col_name`";
